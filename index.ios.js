2e9e02763a7a76525a09982993adfaba1e259094
Lauxx <lm.nichols@hotmail.com>
2016-08-03 11:31:14 -0600

    now able to flip between views

--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 //View = container; Text, Image = able to create some static information
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, Navigator, TouchableHighlight } from 'react-native';
 
-var AwesomeProject = React.createClass({
-  render: function(){
-    var pic = {
-      uri: 'https://i.ytimg.com/vi/-szWTcQa8n8/maxresdefault.jpg'
-    };
 
+export default class NavAllDay extends Component {
+  
+
+  render() {
     return (
-      <View style={{flex: 1}}>
+      <Navigator
+        initialRoute = {{ id: 'Welcome'}}
+        renderScene = { this.navigatorRenderScene }
+        configureScene = {(route, routeStack) => 
+          Navigator.SceneConfigs.FloatFromRight}
         
-        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
-          <Text style={styles.welcome}>
-            Welcome Lauren.
-          </Text>
+      />
+    );
+  }
+
+  navigatorRenderScene(route, navigator){
+    _navigator = navigator;
+    switch(route.id){
+      case 'Welcome':
+        return (<Welcome navigator={ navigator } title="Welcome" />);
+      case 'Second':
+        return (<Second navigator={ navigator } title="Second" /> );  
+    }
+  }
+
+};
+
+class Welcome extends Component {
+  onButtonPress(){
+    this.props.navigator.push({
+      id: 'Second'
+    });
+  }
+
+  render(){
+    return (
+        <View style = { styles.container } >
+          <TouchableHighlight onPress = {this.onButtonPress.bind(this)}>
+            <Text> Welcome to my IOS App </Text>
+          </TouchableHighlight> 
+          <Text  style = { styles.welcome }> 
+            Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla.
+
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod.</Text>
         </View>
-        <View style={{flex: 3, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 1, backgroundColor: 'steelblue'}} />
-        
-       </View> 
       )
   }
-});
+};
+
+class Second extends Component {
+  onButtonPress(){
+    this.props.navigator.push({
+      id: 'Welcome'
+    });
+  }
+
+  render(){
+    return (
+      <View style = { styles.container }> 
+        <TouchableHighlight onPress = {this.onButtonPress.bind(this)}>
+            <Text> This is our second page. </Text>
+          </TouchableHighlight> 
+          <Text style = { styles.welcome }> 
+            Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla.
+
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod.</Text>
+      </View>
+      )
+  }
+}
+
+
 
 
 const styles = StyleSheet.create({
@@ -37,15 +84,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'grey',
-    borderColor: 'black',
-    borderWidth: 4,
+    backgroundColor: 'powderblue',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+    fontSize: 16,
+    textAlign: 'justify',
     marginTop: 50,
-    fontFamily: 'Verdana',
     padding: 10,
   },
   picture: {
@@ -59,4 +103,7 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('awesomeproject', () => AwesomeProject);
+AppRegistry.registerComponent('awesomeproject', () => NavAllDay);
+
+
+
